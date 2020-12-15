948e335e9bf27aa529cb3f3668d0b34c2854c2e3
pmendl <pavel_mendl@centrum.cz>
2020-12-15 12:42:31 +0100

    Corrected previously somehow generically grown system reports

--- Login/login.php
+++ Login/login.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-/*	echo "<div class=\"system-report\">ACTION=${_POST['action']}<div>"; */
 	
 if($_POST["action"] == "CLEAR") {
 	unset($_SESSION["user_id"]);
@@ -42,17 +41,15 @@ if($_POST["action"] == "CLEAR") {
 			if (isset(Database::$error_message)) {
 				$error_message = $servername . ":" . $port . "&emsp;" . Database::$error_message;
 			} else {
-				if (is_object(Database::$conn)) {
-					
-	//				echo Database::$conn->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "???";
-	//				echo Database::DB_USER;
-				} else {
-					echo "NOT OBJECT ";
+				if (!is_object(Database::$conn)) {
+					echo "NOT OBJECT Database::\$conn </br>";
+					$error_message = "login.php: Database::\$conn is not object ";
 				}
 				print_r($_POST);
 				echo "</br>";
-				if(Database::$conn == FALSE) {
-					echo "DB FAILED! ";
+				if(!Database::$conn) {
+					echo "DB FAILED! Database::\$conn </br>";
+					$error_message = "login.php: Database::\$conn is not object ";
 				} else {
 					$driver_name =Database::$conn->getAttribute(PDO::ATTR_DRIVER_NAME);
 			    	$conn_stat =Database::$conn->getAttribute(PDO::ATTR_CONNECTION_STATUS);
@@ -60,30 +57,32 @@ if($_POST["action"] == "CLEAR") {
 					
 					
 					$select = Database::$conn->prepare("SELECT id, password_hash FROM user WHERE username = ?");
-				} 
-	//			echo "<div>TEST3</div>";
-				if (!$select->execute(array($_POST["username"]))) {
-					$error_message = "Selhal dotaz SELECT na uživatele.";
-					echo  $error_message; 
-				} else {
-					$result=$select->fetch(PDO::FETCH_ASSOC);
-	//				echo $result;
-					if (!$result) {
-						echo "Zadaný uživatel nemá oprávnění užívat tento systém. Prosím, zkontrolujte své přihlašovací údaje.";
+
+//					echo "<div>TEST3</div>";
+//					echo  "\$error_message = $error_message";
+				
+					if (!$select->execute(array($_POST["username"]))) {
+						$error_message = "Selhal dotaz SELECT na uživatele.";
 					} else {
-	//					echo '<div class="system-report">';
-						print_r($result);
-	//					echo  "</br>---</br>"."TEST </div>";
-						if($result[password_hash]) {
-	//						echo "TODO: verify password";
+						$result=$select->fetch(PDO::FETCH_ASSOC);
+//						echo $result;
+						if (!$result) {
+							echo "Zadaný uživatel nemá oprávnění užívat tento systém. Prosím, zkontrolujte své přihlašovací údaje.";
 						} else {
-							$password_change="new";
-	//						echo <<<EOT
-	//						TODO: set new password
-	//EOT; 
+//							echo '<div class="system-report">';
+							print_r($result);
+//							echo  "</br>---</br>"."TEST </div>";
+							if($result[password_hash]) {
+//								echo "TODO: verify password";
+							} else {
+								$password_change="new";
+//								echo <<<EOT
+//								TODO: set new password
+//EOT; 
+							}
 						}
+						
 					}
-					
 				}
 			}
 			echo '</div>';
@@ -92,44 +91,44 @@ if($_POST["action"] == "CLEAR") {
 		if (isset($error_message)) {
 			echo "<div><p> Došlo k interní chybě systému. Prosím informujte správce systému  
 			na telefonu 602 645 347 a sdělte mu následující chybu:</p>"; 
-			echo '<p style="background:yellow">' . $error_message . "<\p></div>";
-		}
-		
-		if ($password_change) {
-			echo <<<EOT
-			<form action="/Login/password_change.php" method="post">  
-				<div class="login-rows">
-					<table>
-EOT;
-			if ($password_change == "new") {
-				echo <<<EOT
-							<tr><td>Pro první přihlášení si, prosím, zvolte heslo, které budete nadále používat.<td/><tr/>
-							<input type="hidden" name="old_password" value=""/>
-EOT;
-			} else {				
-				echo <<<EOT
-							<tr><td>Zvolte nové heslo:<td/><tr/>
-							<tr><td><label for="old_password">Původní heslo:</label></td></tr>
-							<tr><td><input type="password" name="old_password" /></td></tr>
-EOT;
-			}
-				echo <<<EOT
-							<tr><td><label for="password1">Nové heslo:</label></td></tr>
-							<tr><td><input type="password" id="password1" name="password1" oninput="myUpdate(event)" /></td><td><span id="passNotNull" class="unmet-login-condition">Nesmí být prázdné</td></span></tr>
-							<tr><td><label for="password2">Znovu pro potvrzení:</label></td></tr>
-							<tr><td><input type="password" id="password2" name="password2" oninput="myUpdate(event)" /></td><td><span id="passEqual" class="unmet-login-condition">Musí se shodovat</span></td></tr>
-							<tr><td><button id="password_send" type="submit" disabled />Odeslat</button><button type="button" onclick="reloadIndex()">Zrušit</button></td><td></td></tr>
-					</table>
-				</div>
-				
-				
-EOT;
+			echo '<p style="background:yellow">' . $error_message . "</p></div>";
 		} else {
-			echo '<button type="button" onclick="reloadIndex()">OK</button>';
-		}
 		
+			if ($password_change) {
+				echo <<<EOT
+				<form action="/Login/password_change.php" method="post">  
+					<div class="login-rows">
+						<table>
+EOT;
+				if ($password_change == "new") {
+					echo <<<EOT
+								<tr><td>Pro první přihlášení si, prosím, zvolte heslo, které budete nadále používat.<td/><tr/>
+								<input type="hidden" name="old_password" value=""/>
+EOT;
+				} else {				
+					echo <<<EOT
+								<tr><td>Zvolte nové heslo:<td/><tr/>
+								<tr><td><label for="old_password">Původní heslo:</label></td></tr>
+								<tr><td><input type="password" name="old_password" /></td></tr>
+EOT;
+				}
+					echo <<<EOT
+								<tr><td><label for="password1">Nové heslo:</label></td></tr>
+								<tr><td><input type="password" id="password1" name="password1" oninput="myUpdate(event)" /></td><td><span id="passNotNull" class="unmet-login-condition">Nesmí být prázdné</td></span></tr>
+								<tr><td><label for="password2">Znovu pro potvrzení:</label></td></tr>
+								<tr><td><input type="password" id="password2" name="password2" oninput="myUpdate(event)" /></td><td><span id="passEqual" class="unmet-login-condition">Musí se shodovat</span></td></tr>
+								<tr><td><button id="password_send" type="submit" disabled />Odeslat</button><button type="button" onclick="reloadIndex()">Zrušit</button></td><td></td></tr>
+						</table>
+					</div>
+					
+					
+EOT;
+			} else {
+				echo '<button type="button" onclick="reloadIndex()">OK</button>';
+			}
 			
-		?>
+				
+			?>
 		</div>		
 		<script language="javascript" type="text/javascript" src="/Tools/footer.js"></script>
 		<script>
@@ -153,5 +152,6 @@ EOT;
 				}
 			}
 		</script>
+		<?php } ?>
 	</body>
 </html>	
