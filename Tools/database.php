859fb2e1aad36d0ee2b2f6440aaa38a52db1ed5b
pmendl <pavel_mendl@centrum.cz>
2020-12-22 22:30:48 +0100

    Added SQL insertion safety measure
    self::$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
    according to https://stackoverflow.com/questions/60174/how-can-i-prevent-sql-injection-in-php?rq=1

--- Tools/database.php
+++ Tools/database.php
@@ -6,27 +6,24 @@ class Database
 
 	static public function DbOpen() {
 
-		try {
-			// Parse with sections
-			$ini_array = parse_ini_file("database.ini",true);
-
-			// Create connection from parsed data			
-		    self::$conn = new PDO("mysql:host=".$ini_array["Database"]["DB_SERVER"].
-		    	";port=".$ini_array["Database"]["DB_PORT"].";dbname=".$ini_array["Database"]["DB_NAME"], 
-		    	$ini_array["Database"]["DB_USER"], $ini_array["Database"]["DB_PASSWORD"]);
-
-		    // set the PDO error mode to exception
-		    self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-/*			
-	    	$driver_name = self::$conn->getAttribute(PDO::ATTR_DRIVER_NAME);
-		    $conn_stat = self::$conn->getAttribute(PDO::ATTR_CONNECTION_STATUS);
-			echo "Database: Connected successfully: $driver_name at $conn_stat</br>";
+		// Parse with sections
+		$ini_array = parse_ini_file("database.ini",true);
+	
+		// Create connection from parsed data			
+	    self::$conn = new PDO("mysql:host=".$ini_array["Database"]["DB_SERVER"].
+	    	";port=".$ini_array["Database"]["DB_PORT"].";dbname=".$ini_array["Database"]["DB_NAME"], 
+	    	$ini_array["Database"]["DB_USER"], $ini_array["Database"]["DB_PASSWORD"]);
+	
+		// CRITICAL: make sure PHP does not preprocess prepared queries to be safe about SQL insertion
+		self::$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	    // set the PDO error mode to exception
+		self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+/*		CODE FOR DEBUG PURPOSES		
+		$driver_name = self::$conn->getAttribute(PDO::ATTR_DRIVER_NAME);
+	    $conn_stat = self::$conn->getAttribute(PDO::ATTR_CONNECTION_STATUS);
+		echo "Database: Connected successfully: $driver_name at $conn_stat</br>";
 */
-		}
-		catch(PDOException $e) {
-			self::$error_message = $e->getMessage();
-			echo "DB ERROR";
-		}
 		
 		return $conn;
 	}
