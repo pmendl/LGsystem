5cb634a4f2b8f0b15017c084977d282fe96503c3
pmendl <pavel_mendl@centrum.cz>
2021-01-16 11:07:09 +0100

    Login form moved to login.php Thorough revision and code cleaning still needed

--- Login/login.php
+++ Login/login.php
@@ -9,19 +9,16 @@ try {
 	unset($h);
 // --- PAGE CODE START ---
 ?>
+	<div class="main-box">
+		<div class="system-report">
 
 <?php
-if($_POST["action"] == "CLEAR") {
-	unset($_SESSION["user_id"]);
-}
-?>
-		<div class="main-box">
-			<div class="system-report">
-		
-		<?php
 
-		print_r($_POST);
-		if($_POST["action"] == "LOGIN") {
+	switch ($_POST['action']) {
+		case 'CLEAR':
+			unset($_SESSION["user_id"]);
+		case 'LOGIN':
+			print_r($_POST);
 			require("../Tools/database.php");
 
 			$db=new Database("database.ini",true);
@@ -60,11 +57,9 @@ if($_POST["action"] == "CLEAR") {
 				}
 				echo "</div>\n";
 			}
-		}
 		
-		
-		if ($password_change) {
-			echo <<<EOT
+			if ($password_change) {
+				echo <<<EOT
 				<form action="/Login/password_change.php" method="post">  
 					<div class="login-rows">
 						<table>
@@ -95,8 +90,42 @@ EOT;
 				</form>
 			
 EOT;
-		} else {
-			echo '<button type="button" onclick="reloadIndex()">OK</button>';
+			}
+			break;
+			
+			
+		default:
+//			echo '<button type="button" onclick="reloadIndex()">OK</button>';
+?>
+<div class="main-box">
+		<form action="/Login/login.php" method="post">
+<?php
+			if(array_key_exists("user_id", $_SESSION)) { ?>
+				SESSION_USER_ID = <?php echo $_SESSION["user_id"]; ?><br/><br/>
+				<button type="submit" name="action" value="CLEAR">Odhlášení</button>
+<?php } else { ?>
+				<div class="to-be-centred">Prosím, přihlaste se:</div>
+				
+				<div class="login-rows">
+					<table>
+						<tr>
+							<td><label for="username">Uživatel:</label></td>
+							<td><label for="password">Heslo:</label></td>
+						</tr>
+						<tr>
+							<td><input type="text" name="username" /></td>
+							<td><input type="text" name="password" /></td>
+						</tr>
+					</table>
+				</div>
+				<div class="to-be-centred">
+					<button type="submit" name="action" value="LOGIN">Přihlášení</button>
+				</div>
+<?php
+			}
+?>
+		</form>
+	</div>
 		}
 		echo "</div></div>\n";
 		?>
@@ -122,6 +151,7 @@ EOT;
 			}
 		</script>
 <?php
+	}
 // --- PAGE CODE END ---
 	include "../Tools/footer.html";
 	ob_end_flush();
